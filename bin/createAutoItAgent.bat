fd212a5475d7cd9855bf5ef07e6c97663c370c04
kobihk <kobihk@gmail.com>
2013-07-03 15:25:15 +0300

    Fixed the jars name with the new names and paths

--- bin/createAutoItAgent.bat
+++ bin/createAutoItAgent.bat
@@ -9,14 +9,15 @@ rem Copy files
 echo Copy files to %1%\lib...
 copy %RUNNER_ROOT%\lib\j2autoit.jar %1%\lib
 copy %RUNNER_ROOT%\lib\jsystemCore.jar %1%\lib
-copy %RUNNER_ROOT%\lib\filetransfer.jar %1%\lib
-copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-codec-1.3.jar %1%\lib
-copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-io-1.3.1.jar %1%\lib
-copy %RUNNER_ROOT%\thirdparty\lib\commons-logging-api.jar %1%\lib
+copy %RUNNER_ROOT%\lib\FileTransfer.jar %1%\lib
+copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-codec.jar %1%\lib
+copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-io.jar %1%\lib
+copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-logging-api.jar %1%\lib
 copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-net.jar %1%\lib
+copy %RUNNER_ROOT%\thirdparty\CommonLib\commons-compress.jar %1%\lib
 copy %RUNNER_ROOT%\thirdparty\CommonLib\jakarta-oro-2.0.8.jar %1%\lib
-copy %RUNNER_ROOT%\thirdparty\lib\xml-apis.jar %1%\lib
-copy %RUNNER_ROOT%\thirdparty\lib\xmlrpc-2.0.jar %1%\lib
+copy %RUNNER_ROOT%\thirdparty\CommonLib\xml-apis.jar %1%\lib
+copy %RUNNER_ROOT%\thirdparty\CommonLib\xmlrpc-2.0.jar %1%\lib
 
 echo Copy batch file to %1%\bin
 copy %RUNNER_ROOT%\runAutoItAgent.bat %1%\bin
